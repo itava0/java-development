c71736176d5f79bc80f604daf89593c9ff77f41b
Ivan Tavarez <ivantavarez2@gmail.com>
2023-10-04 10:52:52 -0400

    Created a ShoppingList

---
+++ workbook-1/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        String[] shoppingList = {"egg", "milk", "cereal", "rice", "beans", "soda", "chips","beer", "platains","yogurt"};
+
+        for (int i = 0; i < shoppingList.length; i++) {
+            System.out.println(shoppingList[i]);
+        }
+    }
+}
