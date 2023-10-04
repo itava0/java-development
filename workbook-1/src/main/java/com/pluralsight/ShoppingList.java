09215534255c41477cd65661a1051b35c218917f
Ivan Tavarez <ivantavarez2@gmail.com>
2023-10-04 10:58:05 -0400

    completed ShoppingList app

--- workbook-1/src/main/java/com/pluralsight/ShoppingList.java
+++ workbook-1/src/main/java/com/pluralsight/ShoppingList.java
@@ -4,8 +4,8 @@ public class ShoppingList {
     public static void main(String[] args) {
         String[] shoppingList = {"egg", "milk", "cereal", "rice", "beans", "soda", "chips","beer", "platains","yogurt"};
 
-        for (int i = 0; i < shoppingList.length; i++) {
-            System.out.println(shoppingList[i]);
+        for (String s : shoppingList) {
+            System.out.println(s);
         }
     }
 }
